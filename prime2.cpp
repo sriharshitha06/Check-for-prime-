3587706d412ed55de39fe27b0c88204beac39b8b
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-22 23:30:00 +0530

    Create prime2.cpp

---
+++ prime2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+bool checkPrime(int n){ 
+    int cnt = 0;
+    for(int i = 1; i<=n; i++){ 
+        if(n%i ==0){ 
+            cnt = cnt+1;
+        }
+    }
+    if(cnt==2){
+        return true;
+    }
+    else{ 
+        return false; 
+    }
+}
+int main() {
+    int n;
+    cin>>n;
+    bool isPrime = checkPrime(n);
+    if(isPrime){
+        cout << n << " is a prime number." << endl;
+    } else {
+        cout << n << " is not a prime number." << endl;
+    }
+    return 0;
+}
+                                
+                            
