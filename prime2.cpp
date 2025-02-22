5fc7d6f895283e0918f44afa81a4c9b2d9d9fc4f
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-23 00:05:23 +0530

    Update prime2.cpp

--- prime2.cpp
+++ prime2.cpp
@@ -1,30 +1,31 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
-bool checkPrime(int n){ 
-    int cnt = 0;
-    for(int i = 1; i<=n; i++){ 
-        if(n%i ==0){ 
-            cnt = cnt+1;
+
+// Check if n is a prime number
+bool checkPrime(int n) {
+    int cnt = 0; // Counter for divisors
+
+    // Loop through all numbers from 1 to n
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) { // If i is a divisor
+            cnt++; // Increment count
         }
     }
-    if(cnt==2){
-        return true;
-    }
-    else{ 
-        return false; 
-    }
+
+    return (cnt == 2); // True if exactly 2 divisors (1 and n)
 }
+
 int main() {
-    int n;
-    cin>>n;
-    bool isPrime = checkPrime(n);
-    if(isPrime){
+    int n; // Input number
+    cin >> n; // Read input
+
+    bool isPrime = checkPrime(n); // Check if n is prime
+    if (isPrime) {
         cout << n << " is a prime number." << endl;
     } else {
-        cout << n << " is not a prime number." << endl;
+        cout << n << " is not a prime number." << endl; 
     }
-    return 0;
+
+    return 0; 
 }
-                                
-                            
