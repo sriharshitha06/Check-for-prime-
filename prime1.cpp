d0084fe0a0b0d378e03f5ed7e679295673513645
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-23 00:01:51 +0530

    Update prime1.cpp

--- prime1.cpp
+++ prime1.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h> 
 #include <iostream>
 using namespace std;
-bool checkPrime(int n){ 
-    int cnt = 0;
-    for(int i = 1; i <= sqrt(n); i++){ 
-        if(n % i == 0){ 
-            cnt = cnt + 1;
-            if(n / i != i){
-                cnt = cnt + 1;
+
+// Check if n is a prime number
+bool checkPrime(int n) {
+    int cnt = 0; // Counter for divisors
+
+    // Loop through possible divisors
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) { // If i is a divisor
+            cnt++; // Increment count
+            if (n / i != i) { // Check for distinct divisor
+                cnt++; // Increment count for the paired divisor
             }
         }
     }
-    if(cnt == 2){
-        return true;
-    }
-    else{ 
-        return false; 
-    }
+
+    return (cnt == 2); // Return true if prime (exactly 2 divisors)
 }
+
 int main() {
-    int n;
-    cin>>n;
-    bool isPrime = checkPrime(n);
-    if(isPrime){
-        cout << n << " is a prime number." << endl;
+    int n; // Input number
+    cin >> n; // Read input
+
+    bool isPrime = checkPrime(n); // Check if n is prime
+    if (isPrime) {
+        cout << n << " is a prime number." << endl; 
     } else {
-        cout << n << " is not a prime number." << endl;
+        cout << n << " is not a prime number." << endl; 
     }
-    return 0;
+    
+    return 0; 
 }
-                                
-                            
